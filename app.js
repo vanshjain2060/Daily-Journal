146f06e4fb0ed5462e7e7f0decf700925e362990
vanshjain2060 <vanshjain0311@gmail.com>
2024-05-21 19:39:40 +0530

    DataBase Connected

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require('mongoose');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Click on the Compose Button to post your new Blog on this page.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -16,10 +17,26 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = [];
 
-app.get("/", function(req, res) {
-  res.render("home" , { StartingContent : homeStartingContent , posts : posts});
+const dbURI = 'mongodb+srv://admin-vansh:vanshjain2060@cluster0.ki3p5of.mongodb.net/blogDB';
+// Connect to MongoDB
+mongoose.connect(dbURI, { useNewUrlParser: true});
+
+const postSchema = {
+  title : String,
+  content : String
+}
+
+const Post = mongoose.model("Post" , postSchema);
+
+app.get('/', async function(req, res) {
+  try {
+    const posts = await Post.find(); // Fetch all posts
+    res.render('home', {StartingContent : homeStartingContent , posts : posts}); // Render 'home.ejs' with the data
+  } catch (err) {
+    console.error('Error fetching posts:', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 
@@ -34,15 +51,12 @@ app.get("/contact" , function(req, res) {
 
 
 // this is a general route with using express routing params
-app.get("/posts/:topic" , function(req, res) {
-  let element = req.params.topic;
-  element = _.lowerCase(element);
-  posts.forEach(function(post) {
-    let storedElement = _.lowerCase(post.title);
-    if(storedElement === element) {
-      res.render("post" , {title : post.title , content : post.content});
-    }
-  });
+app.get("/posts/:postId" ,async function(req, res) {
+  let element = req.params.postId;
+  const post = await Post.findOne({ _id: element});
+  if(post) {
+    res.render("post", {title : post.title, content : post.content});
+  }
 });
 
 
@@ -52,14 +66,16 @@ app.get("/compose" , function(req, res) {
 });
 
 app.post("/compose" , function(req, res) {
-  const post = {
+  const post = new Post ({
     title : req.body.postTitle, 
     content : req.body.postBody 
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+  post.save(function(err) {
+    if(!err) res.redirect("/");
+  });
 });
 
-app.listen(3000, function() {
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
